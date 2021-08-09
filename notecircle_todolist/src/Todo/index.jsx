20feba568a6e6a0952bc3feac6242e34e44899dc
joohyeson <juhye.son990902@gmail.com>
2021-08-09 21:17:02 +0900

    Add #2 React-Bootstrap 추가

--- notecircle_todolist/src/Todo/index.jsx
+++ notecircle_todolist/src/Todo/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import "./Todo.css";
 
 function Todo(props) {
@@ -20,7 +21,7 @@ function Todo(props) {
   return (
     <div className="container-center-horizontal">
       <div className="mein-hwamyeon-todo screen">
-        <div className="navbar">
+        <div className="navbar_m">
           <div className="todo">
             <div className="overlap-group" style={{ backgroundImage: `url(${overlapGroup})` }}>
               <div className="todo-1 valign-text-middle lato-normal-jacarta-18px">{todo}</div>
@@ -43,21 +44,34 @@ function Todo(props) {
           </div>
           <div className="text-1 leaguegothic-regular-normal-white-64px">{text1}</div>
         </div>
-        <div className="todotext">
-          <div className="flex-row">
-            <div className="rectangle-10"></div>
-            <div className="text valign-text-middle lato-normal-jacarta-18px">{text}</div>
-          </div>
-          <img className="line-5" src={line5} />
-        </div>
+        <Container>
+          <Row>
+            <Col><테이블내용 text={text} src={line5} /></Col>
+            <Col><테이블내용 text={text} src={line5} /></Col>
+          </Row>
+        </Container>
+
         <div className="addbutton">
           <div className="overlap-group4">
             <div className="text-2 valign-text-middle leaguegothic-regular-normal-white-64px">{text2}</div>
           </div>
         </div>
       </div>
-    </div>
+
+    </div >
   );
+}
+
+function 테이블내용(props) {
+  return (
+    <div className="todotext">
+      <div className="flex-row">
+        <div className="rectangle-10"></div>
+        <div className="text valign-text-middle lato-normal-jacarta-18px">{props.text}</div>
+      </div>
+      <img className="line-5" src={props.src} />
+    </div>
+  )
 }
 
 export default Todo;
